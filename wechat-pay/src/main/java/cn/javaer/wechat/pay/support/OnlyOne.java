be1fe2b49550c4bde83f36f60decc20c67ff0590
cn-src <public@javaer.cn>
2018-03-09 15:58:20 +0800

    校验参数

--- wechat-pay/src/main/java/cn/javaer/wechat/pay/support/OnlyOne.java
+++ wechat-pay/src/main/java/cn/javaer/wechat/pay/support/OnlyOne.java
@@ -13,27 +13,18 @@
 
 package cn.javaer.wechat.pay.support;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * @author zhangpeng
  */
-@Target({TYPE, ANNOTATION_TYPE})
+@Target({FIELD})
 @Retention(RUNTIME)
-@Constraint(validatedBy = {ValidOrderQueryRequestValidator.class})
 @Documented
-public @interface ValidOrderQueryRequest {
-    String message() default "{cn.javaer.wechat.pay.support.ValidUnifiedOrderRequest.message}";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
+public @interface OnlyOne {
 }
---
+++ wechat-pay/src/main/java/cn/javaer/wechat/pay/support/OnlyOneNotNullValidator.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2018 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.javaer.wechat.pay.support;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author zhangpeng
+ */
+public class OnlyOneNotNullValidator implements ConstraintValidator<OnlyOneNotNull, Object> {
+
+    private static final Map<Class, List<Field>> CACHE_FOR_SIGN = new ConcurrentHashMap<>();
+
+    @Override
+    public void initialize(final OnlyOneNotNull constraintAnnotation) {
+        // nothing
+    }
+
+    @Override
+    public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+
+        final List<Field> fields = CACHE_FOR_SIGN.computeIfAbsent(value.getClass(), clazz0 ->
+                FieldUtils.getFieldsListWithAnnotation(clazz0, OnlyOne.class));
+
+        if (null == fields || fields.isEmpty()) {
+            return true;
+        }
+        int count = 0;
+        try {
+            for (final Field field : fields) {
+                field.setAccessible(true);
+                if (null != field.get(value)) {
+                    count++;
+                }
+            }
+        } catch (final IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (count != 1) {
+            return false;
+        }
+        return false;
+    }
+
+}
