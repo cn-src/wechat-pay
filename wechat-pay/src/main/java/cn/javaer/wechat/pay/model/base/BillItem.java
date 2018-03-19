44378558e9d97add6e96280d23381b0ea363309d
cn-src <public@javaer.cn>
2018-03-19 18:16:32 +0800

    update javadoc

--- wechat-pay/src/main/java/cn/javaer/wechat/pay/model/base/BillItem.java
+++ wechat-pay/src/main/java/cn/javaer/wechat/pay/model/base/BillItem.java
@@ -25,99 +25,99 @@ import lombok.Setter;
 @Setter
 public class BillItem {
     /**
-     * 交易时间
+     * 交易时间.
      */
     private String tradeTime;
     /**
-     * 公众账号ID
+     * 公众账号ID.
      */
     private String appId;
     /**
-     * 商户号
+     * 商户号.
      */
     private String mchId;
     /**
-     * 子商户号
+     * 子商户号.
      */
     private String subMchId;
     /**
-     * 设备号
+     * 设备号.
      */
     private String deviceInfo;
     /**
-     * 微信订单号
+     * 微信订单号.
      */
     private String transactionId;
     /**
-     * 商户订单号
+     * 商户订单号.
      */
     private String outTradeNo;
     /**
-     * 用户标识
+     * 用户标识.
      */
     private String openId;
     /**
-     * 交易类型
+     * 交易类型.
      */
     private String tradeType;
     /**
-     * 交易状态
+     * 交易状态.
      */
     private String tradeState;
     /**
-     * 付款银行
+     * 付款银行.
      */
     private String bankType;
     /**
-     * 货币种类
+     * 货币种类.
      */
     private String feeType;
     /**
-     * 总金额
+     * 总金额.
      */
     private String totalFee;
     /**
-     * 企业红包金额
+     * 企业红包金额.
      */
     private String couponFee;
     /**
-     * 微信退款单号
+     * 微信退款单号.
      */
     private String refundId;
     /**
-     * 商户退款单号
+     * 商户退款单号.
      */
     private String outRefundNo;
     /**
-     * 退款金额
+     * 退款金额.
      */
     private String settlementRefundFee;
     /**
-     * 企业红包退款金额
+     * 企业红包退款金额.
      */
     private String couponRefundFee;
     /**
-     * 退款类型
+     * 退款类型.
      */
     private String refundChannel;
     /**
-     * 退款状态
+     * 退款状态.
      */
     private String refundState;
     /**
-     * 商品名称
+     * 商品名称.
      */
     private String body;
     /**
-     * 商户数据包
+     * 商户数据包.
      */
     private String attach;
     /**
-     * 手续费
+     * 手续费.
      */
     private String poundage;
     /**
-     * 费率
+     * 费率.
      */
     private String poundageRate;
 }
